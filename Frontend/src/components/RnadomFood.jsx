c645e3c19d9603cf7c0409f934eb7a7cf37bd765
myself-viishal <3953vishal@gmail.com>
2024-05-10 19:04:16 +0530

    Remove useless code

--- Frontend/src/components/RnadomFood.jsx
+++ Frontend/src/components/RnadomFood.jsx
@@ -1,29 +1,27 @@
 import React, { useState } from "react";
 import RnadomFoodCard from "./Cards/RnadomFoodCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 function RandomFood() {
-  const [rerender, setRerender] = useState(false);
-
-  const handleClick = () => {
-    setRerender((prevState) => !prevState); // Toggle the state to trigger rerender
-  };
+  
 
   return (
     <div>
       <p className="text-lg  text-center my-4 underline">
-        Feel adventurous? Try one of these five randomly generated recipes!
+        Feel adventurous? Try one of these five randomly generated recipes!{" "}
+        <FontAwesomeIcon icon={faHeart} className="hover:text-customRed duration-200" />
       </p>
 
       <div className="w-full grid grid-cols-4 grid-rows-2 mb-9 h-[22rem] gap-2">
         <RnadomFoodCard
           isVideo={true}
           classname="col-span-2 row-span-2"
-          rerender={rerender}
         />
-        <RnadomFoodCard rerender={rerender} />
-        <RnadomFoodCard rerender={rerender} />
-        <RnadomFoodCard rerender={rerender} />
-        <RnadomFoodCard rerender={rerender} />
+        <RnadomFoodCard  />
+        <RnadomFoodCard  />
+        <RnadomFoodCard  />
+        <RnadomFoodCard  />
       </div>
     </div>
   );
